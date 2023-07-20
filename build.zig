94332d90f700a8e4adbdf4b537b09ac32f2c0136
Matheus Catarino França <matheus-catarino@hotmail.com>
2023-07-20 12:06:39 -0300

    zig build CI

--- build.zig
+++ build.zig
@@ -1,67 +1,73 @@
-//! Requires: zig nightly (ver: 0.11.0-dev, build: 1580 or higher)
-
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "list_cpu_features",
+    const lib_cpu = b.addStaticLibrary(.{
+        .name = "cpu_features",
         .target = target,
         .optimize = optimize,
     });
-    exe.addIncludePath("include");
-    exe.addIncludePath("incude/internal");
-    exe.addCSourceFiles(&.{
+    lib_cpu.addIncludePath("include");
+    lib_cpu.addIncludePath("incude/internal");
+    lib_cpu.addCSourceFiles(&.{
         "src/filesystem.c",
         "src/stack_line_reader.c",
         "src/string_view.c",
-        "src/impl_x86_linux_or_android.c",
-        //"src/impl_x86__base_implementation.c",
-        "src/utils/list_cpu_features.c",
-    }, &.{
-        "-std=c99",
-        "-Wall",
-        "-Wextra",
-        "-Wmissing-declarations",
-        "-Wmissing-prototypes",
-        "-Wno-implicit-fallthrough",
-        "-Wno-unused-function",
-        "-Wold-style-definition",
-        "-Wshadow",
-        "-Wsign-compare",
-        "-Wstrict-prototypes",
-        "-fno-sanitize=all",
-        "-DSTACK_LINE_READER_BUFFER_SIZE=1024",
-        "-DCPU_FEATURES_ARCH_X86",
+        switch (target.getCpuArch()) {
+            .aarch64_32 => "src/impl_arm_linux_or_android.c",
+            .aarch64 => switch (target.getOsTag()) {
+                .windows => "src/impl_aarch64_windows.c",
+                .macos => @panic("unavaliable"),
+                else => "src/impl_aarch64_linux_or_android.c",
+            },
+            .x86, .x86_64 => switch (target.getOsTag()) {
+                .windows => "src/impl_x86_windows.c",
+                .macos => "src/impl_x86_macos.c",
+                .linux => "src/impl_x86_linux_or_android.c",
+                else => "src/impl_x86_freebsd.c",
+            },
+            .mips, .mipsel => "src/impl_mips_linux_or_android.c",
+            .powerpc, .powerpc64 => "src/impl_ppc_linux.c",
+            .riscv64 => "src/impl_riscv_linux.c",
+            else => @panic("Unknown archtecture"),
+        },
+    }, cflags);
+    if (target.isLinux()) {
+        lib_cpu.addCSourceFile("src/hwcaps.c", cflags);
+        lib_cpu.defineCMacro("HAVE_STRONG_GETAUXVAL", null);
+    }
+    if (target.isDarwin()) lib_cpu.defineCMacro("HAVE_SYSCTLBYNAME", null);
+
+    lib_cpu.linkLibC();
+    lib_cpu.pie = b.option(bool, "pie", "Build library with Position Independent Executable") orelse true;
+    lib_cpu.bundle_compiler_rt = b.option(bool, "compiler_rt", "Build Library with Compiler_rt") orelse true;
+
+    lib_cpu.installHeadersDirectory("include", "");
+    b.installArtifact(lib_cpu);
+
+    buildExe(b, .{
+        .path = "src/utils/list_cpu_features.c",
+        .lib = lib_cpu,
     });
+}
+fn buildExe(b: *std.Build, properties: BuildInfo) void {
+    const exe = b.addExecutable(.{
+        .name = properties.filename(),
+        .target = properties.lib.target,
+        .optimize = properties.lib.optimize,
+    });
+    for (properties.lib.include_dirs.items) |dir| {
+        exe.include_dirs.append(dir) catch {};
+    }
+    exe.addCSourceFile(properties.path, cflags);
+    exe.linkLibrary(properties.lib);
     exe.linkLibC();
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    exe.install();
 
-    // This *creates* a RunStep in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = exe.run();
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
     // This allows the user to pass arguments to the application in the build
@@ -73,19 +79,32 @@ pub fn build(b: *std.Build) void {
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step(properties.filename(), b.fmt("Run the {s} app", .{properties.filename()}));
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing.
-    const exe_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
+
+const BuildInfo = struct {
+    lib: *std.Build.Step.Compile,
+    path: []const u8,
+
+    fn filename(self: BuildInfo) []const u8 {
+        var split = std.mem.split(u8, std.fs.path.basename(self.path), ".");
+        return split.first();
+    }
+};
+
+const cflags = &.{
+    "-std=gnu99",
+    "-Wall",
+    "-Wextra",
+    "-Wmissing-declarations",
+    "-Wmissing-prototypes",
+    "-Wno-implicit-fallthrough",
+    "-Wno-unused-function",
+    "-Wold-style-definition",
+    "-Wshadow",
+    "-Wsign-compare",
+    "-Wstrict-prototypes",
+    "-fno-sanitize=all",
+    "-DSTACK_LINE_READER_BUFFER_SIZE=1024",
+};
